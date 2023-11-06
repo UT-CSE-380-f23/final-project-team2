2cd1cdd03dde32641fad73f2a8fd7baaf9f072ae
Kenneth Meyer <kmeyer2@utexas.edu>
2023-11-06 16:18:38 -0600

    updating files

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,20 @@
 // 11/6/23
+// Kenneth Meyer, Rodrigo Gonzalez, Jenil Shah
+
+#include <fstream>
+#include <stdlib.h>
+#include <time.h>
+#include <cmath>
+#include <iostream>
+
+// variables
+int n_samples;
+
+using namespace std;
+
+
+int main(int n_args,char *argv[]){
+
+    n_samples = atoi(argv[1]);
+
+}
