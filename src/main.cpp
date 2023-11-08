ab3cb66be87d15b89dc658226592e4d3932b0539
Kenneth Meyer <kmeyer2@utexas.edu>
2023-11-08 14:28:02 -0600

    pushing recent changes for rodrigo

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,39 @@
 #include <time.h>
 #include <cmath>
 #include <iostream>
+//#include <boost/program_options.hpp>
+#include "parsing.h"
 
 // variables
 int n_samples;
+int DIM, ORDER, N, VERIFY;
+//SOLVER, MODE;
+
+// used to parse command-line arguments
+//namespace po = boost::program_options;
 
 using namespace std;
 
+/* input options
+
+    --dim : 1, 2 (default 1)
+        Choice between 1D or 2D problems
+    --order : 2, 4 (defaul)
+        Choice between 2nd or 4th-order finite differences
+    --N : int
+        Mesh sizing options
+    --verify : bool ???
+        Option to run in a verification mode
+    --solver : jacobi, gauss
+        Choice between Jacobi or Gauss-Seidel solvers
+    --mode : standard, debug
+        Option to control standard output mode (e.g. standard vs debug)
+*/
 
 int main(int n_args,char *argv[]){
 
     n_samples = atoi(argv[1]);
+
+    cout << n_samples << "\n";
 
 }
---
+++ src/parsing.h
@@ -0,0 +1,69 @@
+/*
+
+    Header file for parsing command line input and parsing logfiles.
+
+*/
+
+#include <fstream>
+#include <stdlib.h>
+#include <time.h>
+#include <cmath>
+#include <iostream>
+#include <boost/program_options.hpp>
+
+// used to parse command-line arguments
+namespace po = boost::program_options;
+
+using namespace std;
+
+// example from here: https://www.boost.org/doc/libs/1_63_0/doc/html/program_options/tutorial.html#idp523371328
+// is used to help code the function below:
+int parse_commandLine_args(int ac, char** av){
+    /*
+        Parses command line arguments, which are as follows:
+        --dim : 1, 2 (default 1)
+            Choice between 1D or 2D problems
+        --order : 2, 4 (defaul)
+            Choice between 2nd or 4th-order finite differences
+        --N : int
+            Mesh sizing options
+        --verify : bool ???
+            Option to run in a verification mode
+        --solver : jacobi, gauss
+            Choice between Jacobi or Gauss-Seidel solvers
+        --mode : standard, debug
+            Option to control standard output mode (e.g. standard vs debug)
+    */
+
+    // declare all supported options - some of these should be made optional
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help", "show help message")
+        ("dim", po::value<int>(), "Set problem dimension, 1 or 2")
+        ("order", po::value<int>(), "Finite Difference order, 2 or 4")
+        ("N", po::value<int>(), "Set mesh size")
+        ("verify", po::value<int>(), "Set verification mode, 0 or 1")
+        ("solver", po::value<std::string>(), "Chose solver, 'jacobi' or 'gauss-seidel'")
+        ("mode", po::value<std::string>(), "Chose mode, 'standard' or 'debug'")
+    ;
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(ac, av, desc), vm);
+    po::notify(vm);
+
+    // return help message and do not run code if --help is given
+    if (vm.count("help")) {
+        cout << desc << "\n";
+        return 1;
+    }
+
+    // set default options for some variables if none are chosen
+    if (vm.count("dim")) {
+        cout << "Solving problem in " << vm["dim"].as<int>() << "D" << "\n";
+    } else {
+        cout << "Must specify problem dimension!!";
+    }
+
+
+    return 0;
+}
