c19205a135295488be0cee967f925f6ab8f8ec6a
Kenneth Meyer <kmeyer299@gmail.com>
2023-11-12 18:30:33 -0600

    successful librvy parsing attempt

--- README.md
+++ README.md
@@ -5,11 +5,26 @@ Authors:  Rodrigo Gonzalez, Kenneth L  Meyer, Jenil Shah
 ## Installation Notes
 todo: check if the code should be able to be built and ran on *any* system, or just lonestar6. I.e. does the `Makefile` just need to work properly for any user on ls6? Or for users beyond ls6 as well?
 
-### Running a libgrvy application:
-The following commands were used to successfully run an input example provided in libgrvy's documentation:
+### Installing libgrvy
+Documentation of a local copy of [libgrvy](https://github.com/hpcsi/grvy) is shown below. The link to the version installed can be found [here](https://github.com/hpcsi/grvy/releases/tag/0.38.0)
+
 ```shell
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/bin/grvy/lib
-g++ -I /usr/bin/grvy/include cpp-input.cpp -L/usr/bin/grvy/lib -lgrvy -o cpp_run_grvy_test
+wget https://github.com/hpcsi/grvy/releases/download/0.38.0/grvy-0.38.0.tar.gz
+cd grvy-0.38.0/
+module load boost # libgrvy is built on boost/it is a required module
+./configure --prefix=$WORK/soft/bin/grvy # choose your installation location, might need to create it yourself? not 100% sure.
+make
+make check
+make install
+```
+
+
+### Running a libgrvy application:
+The following commands were used to successfully run an input example provided in libgrvy's documentation on Lonestar6:
+```shell
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$WORK/soft/bin/grvy/lib
+g++ -I $WORK/soft/bin/grvy/include main.cpp -L$WORK/soft/bin/grvy/lib -lgrvy -o test_parsing
+./test_parsing
 ```
 
 where `/usr/bin` will be replaced with wherever grvy was installed. On ls6, environment variables for the `include` and `lib` paths might already exist.
--- src/parsing.h
+++ src/parsing.h
@@ -6,10 +6,14 @@
 #ifndef parsing
 #define parsing
 
+#include<string>
 #include<cstdio>
 #include<cstdlib>
 #include<iostream>
 #include<grvy.h>
+
+using namespace GRVY;
+using namespace std;
 
 /**
  * @brief Parses runtime options from input.txt file
@@ -18,32 +22,44 @@
  * @param argv input from main()
  * @return int 
  */
-int parse_inputs_grvy(int argc, char **argv){
+class GrvyParser {
+    public:
+        std::string solver;
+        int N, DIM, ORDER;
+        int verify, mode;
+        GrvyParser(int argc, char **argv){
     
-    GRVY_Input_Class iparse; // Input parsing object
+            GRVY_Input_Class iparse; // Input parsing object
 
-    double reyn;
+            // Initialize/read the file 
+            if(! iparse.Open("../heat-input.txt"))
+                exit(1);  
 
-    // Initialize/read the file 
-    if(! iparse.Open("./pecos-input.txt"))
-        exit(1);
+            // Read in option variables
+            if( iparse.Read_Var("verify",&verify,0) )
+                printf("--> %-11s = %i\n","verify",verify);
+        
+            if( iparse.Read_Var("mode",&mode,0) )
+                printf("--> %-11s = %i\n","mode",mode);
+        
+            // Read in domain variables
+            if( iparse.Read_Var("domain/N",&N,3) )
+                printf("--> %-11s = %i\n","N",N);
+        
+            if( iparse.Read_Var("domain/DIM",&DIM,1) )
+                printf("--> %-11s = %i\n","DIM",DIM);
 
-    // Read specific variables (with no default values provided)
+            // Read in solver variables
+            if( iparse.Read_Var("solver/solver",&solver,"gauss-seidel") )
+                cout << "--> solver      = " << solver << endl;
+            if( iparse.Read_Var("solver/ORDER",&ORDER,2) )
+                printf("--> %-11s = %i\n","ORDER",ORDER);
 
-    if( iparse.Read_Var("reyn",&reyn,0.) )
-        printf("--> %-11s = %f\n","reyn",reyn);
-  
-    // Read in option variables
+            // Close file and exit
+            iparse.Close();
 
-    // Read in domain variables
-
-    // Read in solver variables
-
-    // Close file and exit
-    iparse.Close()
-    return 0;
-
-}
+        }
+};
 
 
 // example from here: https://www.boost.org/doc/libs/1_63_0/doc/html/program_options/tutorial.html#idp523371328
@@ -55,9 +71,10 @@ int parse_inputs_grvy(int argc, char **argv){
  * @param av input from main program
  * @return int 
  */
+/*
 int deprecated_parsing(int ac, char** av){
 
-    /*
+    
         Parses command line arguments, which are as follows:
         --dim : 1, 2 (default 1)
             Choice between 1D or 2D problems
@@ -71,7 +88,7 @@ int deprecated_parsing(int ac, char** av){
             Choice between Jacobi or Gauss-Seidel solvers
         --mode : standard, debug
             Option to control standard output mode (e.g. standard vs debug)
-    */
+    
 
     // declare all supported options - some of these should be made optional
     po::options_description desc("Allowed options");
@@ -105,5 +122,5 @@ int deprecated_parsing(int ac, char** av){
 
     return 0;
 }
-
+*/
 #endif
