7c66e422f544cc52dac75ce60e343e557a826df5
Kenneth Meyer <kmeyer299@gmail.com>
2023-12-06 14:19:11 -0600

    updating tags of tests to improve output

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,6 +2,7 @@
 
 executable='../src/run'
 
+# bats test_tags=tag:1
 @test "verify ./run exits correctly with invalid DIM" {
 #run $("${factorial_executable} -1")
 run $executable heat-input-bad-dim.txt
@@ -9,6 +10,7 @@ run $executable heat-input-bad-dim.txt
 #[ "${lines[-1]}" = "You don't have a 3D solver implemented yet" ]
 }
 
+# bats test_tags=tag:2
 @test "verify ./run exits correctly with invalid N" {
 #run $("${factorial_executable} -1")
 #run $executable heat-input-bad-N.txt
@@ -17,12 +19,14 @@ run $executable ../heat-input-bad-N.txt
 #[ "${lines[-1]}" = "num_nodes too low!" ]
 }
 
+# bats test_tags=tag:3
 @test "verify ./run.sh exits correctly with invalid order" {
 #run $("${factorial_executable} -1")
 run $executable ../heat-input-bad-order.txt
 [ "$status" -eq 1 ]
 }
 
+# bats test_tags=tag:4
 @test "verify ./run runs correctly in verify mode" {
 #run $("${factorial_executable} -1")
 run $executable ../heat-input-good.txt
