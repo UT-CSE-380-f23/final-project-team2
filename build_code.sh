ae734c48fef1f437c8ea475f38074e6a0516db8b
Kenneth Meyer <kmeyer299@gmail.com>
2023-12-06 23:30:17 -0600

    updating configure.ac to allow for more detailed output

--- build_code.sh
+++ build_code.sh
@@ -8,8 +8,11 @@ module load boost
 module load gsl
 module load petsc
 module load hdf5
+module list 
 
 autoreconf -f -i
+echo $TACC_PHDF5_DIR
+echo $TACC_HDF5_DIR
 
 # could do a better job of searching for this, but, run the configure file
 ./configure CC=mpicc CXX="mpicxx -std=c++17 -O3" FC=mpif90 --with-grvy=/work/03453/villa13/ls6/CSE380/PUBLIC/grvy-intel --with-hdf5=$TACC_PHDF5_DIR --with-petsc=$TACC_PETSC_DIR
